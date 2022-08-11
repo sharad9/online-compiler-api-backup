fe8a29acea8175c1b752f21ed5a0347a7852eb41
Sharad Rakesh Maddheshiya <59807761+sharad9@users.noreply.github.com>
2022-08-11 21:21:57 +0530

    Update executeJava.js

--- executeJava.js
+++ executeJava.js
@@ -1,27 +1,19 @@
-const { exec } = require("child_process");
-const fs = require("fs");
-const path = require("path");
+const { c, cpp, node, python, java } = require('compile-run');
 
-const outputPath = path.join(__dirname, "outputs");
 
-if (!fs.existsSync(outputPath)) {
-	fs.mkdirSync(outputPath, { recursive: true });
-}
-
-const executeJava = (filepath) => {
-	
+const executeJava = async (filepath) => {
+	console.log(filepath);
 	return new Promise((resolve, reject) => {
-		exec(
-			`java ${filepath}`,
-
-			//`javac -d ${outputPath} ${filepath} && java -cp ${outputPath} ${jobId}`,
-			(error, stdout, stderr) => {
-				error && reject({ error, stderr });
-				stderr && reject(stderr);
-				resolve(stdout);
+	
+		java.runFile(filepath, (error, result) => {
+				error && reject({ error, error });
+				result.stderr && reject(result.stderr);
+				resolve(result.stdout);
 			}
-		);
+		)
+		
 	});
+	
 };
 
 module.exports = {
